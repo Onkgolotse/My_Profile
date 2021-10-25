b0c41a9e730c29130ca16f37a7617aff48294d0b
Onkgolotse <81123940+Onkgolotse@users.noreply.github.com>
2021-10-25 09:22:51 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const toplist = document.querySelector('.header .nav-bar .nav-list .toplist');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+toplist.addEventListener('click', () => {
+	toplist.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		toplist.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
