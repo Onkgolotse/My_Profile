559fff2612931d7f34ffdbcd369785a782d6e7da
Onkgolotse <81123940+Onkgolotse@users.noreply.github.com>
2021-10-25 11:12:48 +0200

    java script update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const toplist = document.querySelector('.header .nav-bar .nav-list .toplist');
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-toplist.addEventListener('click', () => {
-	toplist.classList.toggle('active');
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		toplist.classList.toggle('active');
+		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
